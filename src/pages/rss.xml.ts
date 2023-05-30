95ae3bdc095b77830cdaf331d9de4d1f0e599e9c
Deepu Mohan Puthrote <git@deepumohan.com>
2023-05-30 23:52:18 +0100

    Use BASE_URL for constructing links

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,15 +3,16 @@ import { getCollection } from "astro:content";
 
 export async function get() {
   const posts = await getCollection("posts");
+  const { SITE, BASE_URL } = import.meta.env;
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
-    site: "https://my-blog-site.netlify.app",
+    site: `${SITE}/${BASE_URL}`,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.slug}/`,
+      link: `${BASE_URL}posts/${post.slug}/`,
     })),
     customData: `<language>en-gb</language>`,
   });
